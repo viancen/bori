282eeb0ab1705e21c0fdbb18197826e3bc2fe818
Vincent van den Nieuwenhuizen <vincent@inforvision.nl>
2020-09-09 10:27:02 +0200

    Update About.vue

--- resources/js/widgets/About.vue
+++ resources/js/widgets/About.vue
@@ -11,7 +11,15 @@
                 dagverse maaltijd. Maar let op! Omdat Bori staat voor kleinschaligheid bieden de chefs per dag slechts
                 één of twee gerechten aan. En het aantal porties zijn beperkt. Bij Bori staat kwaliteit voorop, niet de
                 kwantiteit.
-                <br/><br/>
+                <ul>
+                    <li>Bori is hét alternatief voor massa en overvloed.</li>
+                    <li>Bori is hét platform voor jong professionals die samenwerken.</li>
+                    <li>Bori is liefde voor eten, liefde voor smaak.</li>
+                    <li>Bori is liefde voor beleving creativiteit.</li>
+                    <li>Bori is liefde voor authenticiteit.</li>
+                    <li>Bori is liefde voor alle culturen.</li>
+                    <li>Bori is lobi!</li>
+                </ul>
                 Plaats dus op tijd uw bestelling zodat u zeker weet dat u Bori-eten heeft!
                 <br /><br />
             </div>
