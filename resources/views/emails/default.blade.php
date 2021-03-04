747acc953ba5d4ee603cb412a5e3991028222633
Vincent van den Nieuwenhuizen <vincent@inforvision.nl>
2021-03-04 09:42:27 +0100

    Update default.blade.php

--- resources/views/emails/default.blade.php
+++ resources/views/emails/default.blade.php
@@ -88,20 +88,20 @@
             }
 
             .btn-primary table td:hover {
-                background-color: #34495e !important;
+                background-color: #000000 !important;
             }
 
             .btn-primary a:hover {
-                background-color: #34495e !important;
-                border-color: #34495e !important;
+                background-color: #000000 !important;
+                border-color: #000000 !important;
             }
         }
     </style>
 </head>
 <body class=""
-      style="background-color: #f6f6f6; font-family: sans-serif; -webkit-font-smoothing: antialiased; font-size: 14px; line-height: 1.4; margin: 0; padding: 0; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%;">
+      style="background-color: #000000; font-family: sans-serif; -webkit-font-smoothing: antialiased; font-size: 14px; line-height: 1.4; margin: 0; padding: 0; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%;">
 <table border="0" cellpadding="0" cellspacing="0" class="body"
-       style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; background-color: #f6f6f6;">
+       style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; background-color: #000000;">
     <tr>
         <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;">&nbsp;</td>
         <td class="container"
